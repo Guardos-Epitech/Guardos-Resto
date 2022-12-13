7bce2082b990e46a12b19e85de84a5f8b09b5082
JosefineM2000 <josefine.mende@epitech.eu>
2022-12-13 17:58:52 +0100

    [FE] [MODIFY] extended add resto form

--- web/src/pages/AddRestaurantPage/AddRestaurantPage.tsx
+++ web/src/pages/AddRestaurantPage/AddRestaurantPage.tsx
@@ -1,27 +1,111 @@
 import React from "react";
 import Header from "@src/components/Header/Header";
 import styles from "@src/pages/AddRestaurantPage/AddRestaurantPage.module.scss";
-import { Container, FormControl, FormHelperText, Input, InputLabel, Paper } from "@mui/material";
+import {
+  Box, Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  OutlinedInput,
+} from "@mui/material";
+import Layout from "../../components/Layout/Layout";
+import placeholderImg from "@src/assets/profile-placeholder.png";
 
 const AddRestaurantPage = () => {
-    return (
-        <div className="test">
-          <Header />
-          <div className={styles.RectOnImg}>
-            <span className={styles.TitleSearch}>My new restaurant</span>
-          </div>
-          <Container maxWidth={"lg"}>
-            <Paper className={styles.ContentAlignment} elevation={3} sx={{ m: 2 }}>
-              {/*TODO: needs to be improved / extended*/}
-              <FormControl>
-                <InputLabel htmlFor="my-input">Email address</InputLabel>
-                <Input id="my-input" aria-describedby="my-helper-text" />
-                <FormHelperText id="my-helper-text">We will never share your email.</FormHelperText>
+  // const [imageSrc, setImageSrc] = React.useState("");
+
+  return (
+    <div className="test">
+      <Header />
+      <div className={styles.RectOnImg}>
+        <span className={styles.TitleSearch}>My new restaurant</span>
+      </div>
+      <Layout>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+          <Grid container>
+            <Grid item xs={3}>
+              <img className={styles.ImageDimensions} src={placeholderImg} alt="Resto Img" />
+              <div className={styles.FormControlMargin}>
+                <FormControl>
+                  <Button className={styles.FormControlMargin} variant="contained" component="label">
+                    Upload Image
+                    <input hidden accept="image/*" multiple type="file" />
+                  </Button>
+                </FormControl>
+              </div>
+            </Grid>
+
+            <Grid container item xs={9}>
+              <Grid item xs={8} className={styles.FieldMargin}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Name</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    // defaultValue="Composed TextField"
+                    label="Name"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={4} className={styles.FieldMargin}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Phone number</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    label="Phone number"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={10}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Street name</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    label="Street name"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={2}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Street number</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    label="Street number"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Postal code</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    label="Postal code"
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={12}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="component-outlined">Country</InputLabel>
+                  <OutlinedInput
+                    id="component-outlined"
+                    label="Country"
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="component-outlined">Description</InputLabel>
+                <OutlinedInput
+                  id="component-outlined"
+                  label="Description"
+                />
               </FormControl>
-            </Paper>
-          </Container>
-        </div>
-    );
+            </Grid>
+          </Grid>
+         </Box>
+      </Layout>
+    </div>
+  );
 };
 
 export default AddRestaurantPage;
