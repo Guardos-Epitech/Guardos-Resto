d4cad75246519202c81a05d560d9538da25e0631
MarcPIster <71755408+MarcPIster@users.noreply.github.com>
2023-01-09 16:50:42 +0100

    [BE] [MERGE] add api to Restaurant BackEnd (#7)
    * [BE] [MODIFY] changed BE from js to ts
    
    * [BE] [MERGE] Added API for dishes and ingredients  (#5)
    
    * [BE] [UPDATE] update linter rules and remove unnecessary files
    
    * [BE] [ADD] add empty .env file
    
    * [BE] [ADD] added ingedients api
    
    * [BE] [ADD] added dishes api
    
    * [BE] [ADD] added "/api/" to routes
    
    * [BE] [ADD] added changeDish method
    
    * [BE] [ADD] added errorhandling if dish or restaurant are unknown
    
    * [BE] [ADD] add api for restauran and handle comments of the pr
    
    * [BE] [MERGE] ADD package-lock to gitignore (#8)
    
    * [BE] [FIX] add package-lock to gitignore
    
    * [BE] [FIX] add package-lock to gitignore
    
    Co-authored-by: wernerramon <ramon.werner@epitech.eu>

---
+++ backend/src/middleware/restaurantMiddleWare.ts
@@ -0,0 +1,19 @@
+import {getRestaurantByName} from '../controllers/restaurantController';
+import mongoose from 'mongoose';
+import {restaurantSchema} from '../models/restaurantInterfaces';
+
+export async function checkIfRestaurantExists(restaurantName: string) {
+    const restaurant = await getRestaurantByName(restaurantName);
+    return !!restaurant;
+}
+
+export async function findMaxIndexRestaurants() {
+    const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+    const restaurants = await Restaurant.find()
+        .sort({ _id: -1 })
+        .limit(1);
+    if (restaurants.length === 0) {
+        return 0;
+    }
+    return restaurants[0]._id;
+}
