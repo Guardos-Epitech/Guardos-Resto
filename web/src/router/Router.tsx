f74c03e87f7bccfaef0043414893af289d8c591d
Alban de Tourtier <71947647+Alban-de-tourtier@users.noreply.github.com>
2023-01-10 12:44:10 +0100

    [FE] add a footer (#10)
    Co-authored-by: azda <da@gmail.com>

--- web/src/router/Router.tsx
+++ web/src/router/Router.tsx
@@ -10,21 +10,24 @@ import AddDishPage from "@src/pages/AddDishPage";
 import EditDishPage from "@src/pages/EditDishPage";
 import EditRestaurantPage from "@src/pages/EditRestaurantPage";
 import AddProductPage from "@src/pages/AddProductPage";
+import AppOutlet from "@src/pages/AppOutlet/AppOutlet";
 
 const MVPRouter = () => {
   return (
     <BrowserRouter>
       <ScrollToTop />
       <Routes>
-        <Route path="addDish" element={<AddDishPage />} />
-        <Route path="addProduct" element={<AddProductPage />} />
-        <Route path="addResto" element={<AddRestaurantPage />} />
-        <Route path="dishes" element={<DishesPage />} />
-        <Route path="editDish" element={<EditDishPage />} />
-        <Route path="editResto" element={<EditRestaurantPage />} />
-        <Route path="products" element={<ProductsPage />} />
-        <Route path="menu" element={<MenuPage />} />
-        <Route path="/" element={<HomePage />}></Route>
+        <Route element={<AppOutlet />}>
+          <Route path="addDish" element={<AddDishPage />} />
+          <Route path="addProduct" element={<AddProductPage />} />
+          <Route path="addResto" element={<AddRestaurantPage />} />
+          <Route path="dishes" element={<DishesPage />} />
+          <Route path="editDish" element={<EditDishPage />} />
+          <Route path="editResto" element={<EditRestaurantPage />} />
+          <Route path="products" element={<ProductsPage />} />
+          <Route path="menu" element={<MenuPage />} />
+          <Route path="/" element={<HomePage />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
