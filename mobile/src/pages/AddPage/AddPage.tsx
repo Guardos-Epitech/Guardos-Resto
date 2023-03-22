b484a047af2345cf0edd47bcb1662dce9a1cc003
Alban-de-tourtier <alban.de-tourtier@epitech.eu>
2023-03-22 16:43:42 +0100

    [CLEAN] clean code

--- mobile/src/pages/AddPage/AddPage.tsx
+++ mobile/src/pages/AddPage/AddPage.tsx
@@ -16,23 +16,11 @@ SplashScreen.preventAutoHideAsync(); // Keep the splash screen visible while we 
 import {
   useFonts,
   Montserrat_100Thin,
-  Montserrat_100Thin_Italic,
-  Montserrat_200ExtraLight,
-  Montserrat_200ExtraLight_Italic,
   Montserrat_300Light,
-  Montserrat_300Light_Italic,
   Montserrat_400Regular,
-  Montserrat_400Regular_Italic,
   Montserrat_500Medium,
-  Montserrat_500Medium_Italic,
   Montserrat_600SemiBold,
-  Montserrat_600SemiBold_Italic,
   Montserrat_700Bold,
-  Montserrat_700Bold_Italic,
-  Montserrat_800ExtraBold,
-  Montserrat_800ExtraBold_Italic,
-  Montserrat_900Black,
-  Montserrat_900Black_Italic,
 } from "@expo-google-fonts/montserrat";
 
 interface Ingr {
@@ -42,23 +30,12 @@ interface Ingr {
 const AddPage = ({ navigation }: { navigation: any }) => {
   let [fontsLoaded] = useFonts({
     Montserrat_100Thin,
-    Montserrat_100Thin_Italic,
-    Montserrat_200ExtraLight,
-    Montserrat_200ExtraLight_Italic,
     Montserrat_300Light,
-    Montserrat_300Light_Italic,
     Montserrat_400Regular,
-    Montserrat_400Regular_Italic,
     Montserrat_500Medium,
-    Montserrat_500Medium_Italic,
     Montserrat_600SemiBold,
-    Montserrat_600SemiBold_Italic,
     Montserrat_700Bold,
-    Montserrat_700Bold_Italic,
-    Montserrat_800ExtraBold,
-    Montserrat_800ExtraBold_Italic,
-    Montserrat_900Black,
-    Montserrat_900Black_Italic,
+
   });
   const [appIsReady, setAppIsReady] = useState(false);
 
