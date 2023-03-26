2b397996e71a8a871b36b0ef62f87cbd35274fde
wernerramon <ramon.werner@epitech.eu>
2023-03-26 21:47:06 +0200

    [BE] [MODIFY] fix origin request blocked error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import logger from 'morgan';
 import path = require('path');
 
 import basicApiIngredients from './routes/ingredients';
-import {connectDataBase, SUCCEED} from './controllers/connectDataBase';
+import { connectDataBase, SUCCEED } from './controllers/connectDataBase';
 import dishes from './routes/dishes';
 import products from './routes/products';
 import restaurants from './routes/restaurants';
@@ -14,21 +14,31 @@ import restaurants from './routes/restaurants';
 async function main() {
   const app = express();
   const port = 8082;
+  const fe = 'http://localhost:8080/';
 
   app.use(logger('dev'));
   app.use(express.json());
-  app.use(express.urlencoded({extended: false}));
+  app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
   app.use(express.static(path.join(__dirname, 'public')));
-  app.use(cors({origin: String('http://localhost:8080/')}));
+  app.use(cors({ origin: String('http://localhost:8080/') }));
 
   const dbStatus = await connectDataBase();
 
   if (dbStatus === SUCCEED) {
     app.listen(port, () => {
-      return console.log(`RestaurantBE is listening at http://localhost:${port}`);
+      return console.log(`RestaurantBE listening at http://localhost:${port}`);
     });
   }
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', fe);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers',
+      'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
 
   app.use('/', basicApiIngredients);
   app.use('/api/products', products);
